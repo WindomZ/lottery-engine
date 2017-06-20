1dd8c7316017b3e1f779a3f9c2181e1dbb1ba4d4
WindomZ <git.windomz@gmail.com>
2017-06-20 17:35:45 +0800

    update(sql): play rule

--- sql/lotterydb.sql
+++ sql/lotterydb.sql
@@ -9,14 +9,14 @@ create table lotterydb.le_play
 	put_time datetime default CURRENT_TIMESTAMP not null,
 	name varchar(32) default '' not null,
 	active tinyint(1) default '1' not null,
-	level int default '0' not null,
 	`desc` varchar(256) default '' not null,
-	rule varchar(256) default '' not null,
+	level int default '0' not null,
 	daily tinyint(1) default '1' not null,
 	`limit` int default '0' not null,
 	size int default '0' not null,
 	count int default '0' not null,
-	weights json not null,
+	weights json,
+	rule tinyint(1) default '0' not null,
 	constraint le_play_id_uindex
 		unique (id)
 )
@@ -63,8 +63,8 @@ create table lotterydb.le_reward
 	put_time datetime default CURRENT_TIMESTAMP not null,
 	name varchar(32) default '' not null,
 	active tinyint(1) default '1' not null,
-	level int default '0' not null,
 	`desc` varchar(256) default '' not null,
+	level int default '0' not null,
 	award_id char(36) not null,
 	award_class int default '0' not null,
 	award_kind int default '0' not null,
@@ -87,3 +87,21 @@ create index le_reward_award_class_award_kind_index
 	on lotterydb.le_reward (award_class, award_kind)
 ;
 
+create table lotterydb.le_rule
+(
+	id char(36) not null,
+	post_time datetime default CURRENT_TIMESTAMP not null,
+	put_time datetime default CURRENT_TIMESTAMP not null,
+	play_id char(36) not null,
+	reward_id char(36) not null,
+	weight int default '0' not null,
+	constraint le_rule_id_uindex
+		unique (id),
+	constraint le_rule_play_id_reward_id_uindex
+		unique (play_id, reward_id)
+)
+;
+
+create index le_rule_play_id_index
+	on lotterydb.le_rule (play_id)
+;
